2e0c2f70505094f3ed705fd1591d4dbda1ff2adc
Lucas Vieira <contatolucas@gmail.com>
2021-01-22 17:12:22 -0300

    Cria stack Athena

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 
-#from athena.stack import AthenaStack
+from athena.stack import AthenaStack
 from aws_cdk import core
 
 from data_platform_bootcamp_teste.data_lake.stack import DataLakeStack
-#from common_stack import CommonStack
 from glue_catalog.stack import GlueCatalogStack
+#from common_stack import CommonStack
 
 app = core.App()
 data_lake = DataLakeStack(app)
 glue = GlueCatalogStack(app, data_lake_bucket=data_lake.data_lake_raw_bucket)
-#athena = AthenaStack(app)
+athena = AthenaStack(app)
 #common = CommonStack(app)
 app.synth()
