0fe56f1d200177a66aec6c18871ef3194e6ad108
Lucas Vieira <contatolucas@gmail.com>
2021-01-22 17:30:09 -0300

    Cria stack Kinesis

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
-#!/usr/bin/env python3
-
 from athena.stack import AthenaStack
 from aws_cdk import core
 
 from data_platform_bootcamp_teste.data_lake.stack import DataLakeStack
 from glue_catalog.stack import GlueCatalogStack
+from kinesis.stack import KinesisStack
+
 #from common_stack import CommonStack
 
 app = core.App()
+
 data_lake = DataLakeStack(app)
-glue = GlueCatalogStack(app, data_lake_bucket=data_lake.data_lake_raw_bucket)
-athena = AthenaStack(app)
-#common = CommonStack(app)
+glue_catalog = GlueCatalogStack(app, data_lake_bucket=data_lake.data_lake_raw_bucket)
+athena_stack = AthenaStack(app)
+kinesis_stack = KinesisStack(app, data_lake_raw_bucket=data_lake.data_lake_raw_bucket)
+#common_stack = CommonStack(app)
+
 app.synth()
